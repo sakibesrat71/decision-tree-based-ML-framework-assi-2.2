a6de73d00f2220f851ee0c0b727a040e795a58ac
Sakib Esrat <83289622+sakibesrat71@users.noreply.github.com>
2026-05-28 04:28:07 +0930

    Add running mean and variance statistics

--- numcompute/stats.py
+++ numcompute/stats.py
@@ -39,3 +39,54 @@ def histogram(x, bins=10):
 def quantiles(x, q):
     x = np.asarray(x, dtype=float)
     return np.quantile(x, q)
+
+
+class StreamingStats:
+    """Track running mean and variance from incoming chunks."""
+
+    def __init__(self):
+        self.count = 0
+        self.mean = None
+        self.m2 = None
+
+    def update_stats(self, X_chunk):
+        X = np.asarray(X_chunk, dtype=float)
+        if X.ndim == 1:
+            X = X.reshape(-1, 1)
+
+        X = X[~np.isnan(X).any(axis=1)]
+        if X.shape[0] == 0:
+            return self
+
+        chunk_count = X.shape[0]
+        chunk_mean = np.mean(X, axis=0)
+        chunk_m2 = np.var(X, axis=0) * chunk_count
+
+        if self.mean is None:
+            self.count = chunk_count
+            self.mean = chunk_mean
+            self.m2 = chunk_m2
+            return self
+
+        total_count = self.count + chunk_count
+        mean_diff = chunk_mean - self.mean
+        self.m2 = self.m2 + chunk_m2 + mean_diff**2 * self.count * chunk_count / total_count
+        self.mean = self.mean + mean_diff * chunk_count / total_count
+        self.count = total_count
+        return self
+
+    def result(self):
+        if self.count == 0:
+            return {"count": 0, "mean": None, "variance": None}
+
+        return {
+            "count": self.count,
+            "mean": self.mean,
+            "variance": self.m2 / self.count,
+        }
+
+
+def update_stats(X_chunk, stats_obj=None):
+    if stats_obj is None:
+        stats_obj = StreamingStats()
+    return stats_obj.update_stats(X_chunk)
